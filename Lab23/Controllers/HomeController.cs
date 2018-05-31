d41a170e137b8a4816234e25785b29562c9253b1
unknown <adamhorn8@gmail.com>
2018-05-31 08:29:00 -0700

    first commit

---
+++ Lab23/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab23.Models;
+
+namespace Lab23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CoffeeEntities ORM = new CoffeeEntities();
+
+            ViewBag.Items = ORM.Items.ToList();
+            ViewBag.Users = ORM.Users.ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            CoffeeEntities ORM = new CoffeeEntities();
+
+            return View();
+        }
+
+        public ActionResult RegSuccess(User data)
+        {
+            CoffeeEntities ORM = new CoffeeEntities();
+
+            ViewBag.Users = ORM.Users.ToList();
+
+            if (ModelState.IsValid)
+            {
+                ORM.Users.Add(data);
+                ORM.SaveChanges();
+                ViewBag.message = $"{data.FirstName} has been added";
+            }
+            else
+            {
+                ViewBag.message = "Item is not valid, cannot add to DB.";
+            }
+
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            CoffeeEntities ORM = new CoffeeEntities();
+
+            ViewBag.Users = ORM.Users.ToList();
+
+            return View();
+        }
+    }
+}
