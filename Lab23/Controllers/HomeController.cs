a026e16222b5cf028dcef389a7d16de8f6bbcbbf
unknown <adamhorn8@gmail.com>
2018-05-31 22:59:20 -0700

    first commit

--- Lab23/Controllers/HomeController.cs
+++ Lab23/Controllers/HomeController.cs
@@ -19,20 +19,6 @@ namespace Lab23.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Register()
         {
             CoffeeEntities ORM = new CoffeeEntities();
